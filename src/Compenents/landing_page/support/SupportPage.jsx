391a24e685f88a6ec1be922a028be086bd65306f
Sarthak <rajjangid3999@gmail.com>
2025-07-17 20:02:07 +0530

    fix issue

--- src/Compenents/landing_page/support/SupportPage.jsx
+++ src/Compenents/landing_page/support/SupportPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from './Hero'
 import CreateTicket from './CreateTicket'
-import  "./supportpage.css"
+import  "./supportPage.css"
 
 export default function SupportPage() {
   return (
